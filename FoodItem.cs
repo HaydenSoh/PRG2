805c2005441dac0da24343d2efb40e032e983c71
HaydenSoh <haydensoh07@gmail.com>
2026-02-04 14:00:47 +0800

    Add files via upload

---
+++ FoodItem.cs
@@ -0,0 +1,33 @@
+﻿//==========================================================
+// Student Number : S10275174
+// Student Name : Ang Zheng Yang
+// Partner Name : Hayden Soh Kai Jun
+//==========================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10275174_PRG2Assignment
+{
+    public class FoodItem
+    {
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public double ItemPrice { get; set; }
+
+        public FoodItem(string name, string description, double price)
+        {
+            ItemName = name;
+            Description = description;
+            ItemPrice = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{ItemName} - ${ItemPrice:F2}";
+        }
+    }
+}
