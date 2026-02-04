9361a7b80b4f40a8ec84ec51f55e8364cb5b9c2c
HaydenSoh <haydensoh07@gmail.com>
2026-02-04 14:38:11 +0800

    Update FoodItem.cs

--- FoodItem.cs
+++ FoodItem.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace S10275174_PRG2Assignment
+namespace S10268816_PRG2Assignment
 {
     public class FoodItem
     {
@@ -33,5 +33,6 @@ namespace S10275174_PRG2Assignment
         }
     }
 }
+
 
 
