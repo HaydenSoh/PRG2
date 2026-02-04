6803673c35a2c6644019e106b6ed37326fcffe7b
HaydenSoh <haydensoh07@gmail.com>
2026-02-04 14:01:09 +0800

    Add files via upload

---
+++ OrderedFoodItem.cs
@@ -0,0 +1,38 @@
+﻿//==========================================================
+// Student Number : S10275174
+// Student Name : Ang Zheng Yang
+// Partner Name : Hayden Soh Kai Jun
+//==========================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10275174_PRG2Assignment
+{
+    public class OrderedFoodItem
+    {
+        public int QtyOrdered { get; set; }
+        public double SubTotal { get; private set; }
+        public FoodItem FoodItem { get; set; }
+
+        public OrderedFoodItem(FoodItem item, int quantity)
+        {
+            FoodItem = item;
+            QtyOrdered = quantity;
+            SubTotal = CalculateSubTotal();
+        }
+
+        public double CalculateSubTotal()
+        {
+            return FoodItem.ItemPrice * QtyOrdered;
+        }
+
+        public override string ToString()
+        {
+            return $"{FoodItem.ItemName}, {QtyOrdered}";
+        }
+    }
+}
